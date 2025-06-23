b0f6bc87c3d5006b046518dad39eaeb0de16c12b
Uyanide <pywang0608@foxmail.com>
2025-06-23 07:15:17 +0200

    queue

--- display.go
+++ display.go
@@ -10,33 +10,45 @@ type Display struct {
 	numLines   int
 	lines      []string
 	outputPath string
+	tail       int
+	size       int
 }
 
 func NewDisplay(numLines int, outputPath string) *Display {
 	if numLines < 1 {
+		log("Invalid number of lines, defaulting to 1")
 		numLines = 1
 	}
 	return &Display{
 		numLines:   numLines,
-		lines:      make([]string, 0, numLines),
+		lines:      make([]string, numLines),
 		outputPath: outputPath,
+		tail:       0,
+		size:       0,
 	}
 }
 
 func (d *Display) Clear() {
-	d.lines = d.lines[:0]
+	d.tail = 0
+	d.size = 0
 	d.display()
 }
 
 func (d *Display) AddLine(line string) {
-	if len(d.lines) >= d.numLines {
-		d.lines = d.lines[1:]
+	d.lines[d.tail%d.numLines] = line
+	d.tail = (d.tail + 1) % d.numLines
+	if d.size < d.numLines {
+		d.size++
 	}
-	d.lines = append(d.lines, line)
 }
 
 func (d *Display) display() {
-	if err := os.WriteFile(d.outputPath, []byte(strings.Join(d.lines, "\n")+"\n"), 0644); err != nil {
+	builder := strings.Builder{}
+	head := d.tail + d.numLines - d.size
+	for i := 0; i < d.size; i++ {
+		builder.WriteString(d.lines[(head+i)%d.numLines] + "\n")
+	}
+	if err := os.WriteFile(d.outputPath, []byte(builder.String()), 0644); err != nil {
 		log(fmt.Sprintf("Error writing to output file: %v", err))
 	}
 }
